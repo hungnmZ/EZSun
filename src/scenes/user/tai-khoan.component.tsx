94f101630ffb114d93d2783ac0a98558529de120
Manh Hung <manhhung99@Manhs-MacBook-Pro.local>
2020-12-11 09:05:34 +0700

    sửa lưu thông tin tk

--- src/scenes/user/tai-khoan.component.tsx
+++ src/scenes/user/tai-khoan.component.tsx
@@ -9,44 +9,20 @@ import { SafeAreaLayout } from '../../components/safe-area-layout.component';
 import { ArrowIosBackIcon } from '../../components/icons';
 import ContentView from '../../layouts/user/tai-khoan';
 import { firebase } from '../../firebase/config';
+import { dateTimeFormat } from '../../util/util';
 
 export const TaiKhoanScreen = ({ navigation }): React.ReactElement => {
     const db = firebase.firestore();
     console.log(db);
     var userId = firebase.auth().currentUser.uid;
     const userRef = db.doc('/users/' + userId);
-        
+
     const [user, setUser] = React.useState(firebase.auth().currentUser);
-    // const [userCreate, setUserCreate] = React.useState({
-    //     email:user.email,
-    //     username: '',
-    //     birthday:'',
-    //     gender:'',
-    //     phoneNumber:''
 
-    // });
-    
-    
-    
-    // const [userCustom, setUserCustom] = React.useState<any>( {
-    //     email:user.email,
-    //     username: '',
-    //     birthday:'',
-    //     gender:'nu',
-    //     phoneNumber:''
-
-    // });
-
-    const [userCustom, setUserCustom] = React.useState<any>( {
-        
-
-    });
-
+    const [userCustom, setUserCustom] = React.useState<any>({});
 
     const [isEdit, setEditBol] = React.useState(false);
     const [isSave, setSaveBol] = React.useState(true);
-
-    
 
     const renderBackAction = (): React.ReactElement => (
         <TopNavigationAction
@@ -66,27 +42,31 @@ export const TaiKhoanScreen = ({ navigation }): React.ReactElement => {
         setSaveBol(true);
         renderOverflowMenuAction();
         setUser(user);
-    
+
         userRef.set({
-            username: userCustom.username,
-            email: userCustom.email,
-            phoneNumber: userCustom.phoneNumber,
-            birthday: userCustom.birthday,
-            gender: userCustom.gender
-        })
+            displayName: userCustom.displayName ? userCustom.displayName : '',
+            email: userCustom.email ? userCustom.email : '',
+            phoneNumber: userCustom.phoneNumber ? userCustom.phoneNumber : '',
+            birthday: userCustom.birthday
+                ? dateTimeFormat(userCustom.birthday)
+                : '',
+            gender: userCustom.gender ? userCustom.gender : '',
+        });
         console.log(userCustom);
 
-        userRef.get().then(function(doc) {
-            if (doc.exists) {
-                console.log("Document data:", doc.data());
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch(function(error) {
-            console.log("Error getting document:", error);
-        });
-        
+        userRef
+            .get()
+            .then(function (doc) {
+                if (doc.exists) {
+                    console.log('Document data:', doc.data());
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log('No such document!');
+                }
+            })
+            .catch(function (error) {
+                console.log('Error getting document:', error);
+            });
     };
 
     const renderOverflowMenuAction = () =>
@@ -94,7 +74,13 @@ export const TaiKhoanScreen = ({ navigation }): React.ReactElement => {
             <React.Fragment>
                 <Button
                     onPress={clickEdit}
-                    style={{ margin: 2, paddingLeft: 18, paddingRight: 18, backgroundColor: '#ff6720', borderColor: '#ff6720' }}
+                    style={{
+                        margin: 2,
+                        paddingLeft: 18,
+                        paddingRight: 18,
+                        backgroundColor: '#ff6720',
+                        borderColor: '#ff6720',
+                    }}
                     size='small'
                 >
                     Edit
@@ -104,7 +90,13 @@ export const TaiKhoanScreen = ({ navigation }): React.ReactElement => {
             <React.Fragment>
                 <Button
                     onPress={clickSave}
-                    style={{ margin: 2, paddingLeft: 15, paddingRight: 15, backgroundColor: '#ff6720', borderColor: '#ff6720' }}
+                    style={{
+                        margin: 2,
+                        paddingLeft: 15,
+                        paddingRight: 15,
+                        backgroundColor: '#ff6720',
+                        borderColor: '#ff6720',
+                    }}
                     size='small'
                 >
                     Save
@@ -112,26 +104,22 @@ export const TaiKhoanScreen = ({ navigation }): React.ReactElement => {
             </React.Fragment>
         );
 
-        
     React.useEffect(() => {
-        
         const fetchData = async () => {
-            
             let doc = await userRef.get();
-            let userCreate: any= {};
+            let userCreate: any = {};
             userCreate.email = doc.data().email;
-            userCreate.username = doc.data().username;
+            userCreate.displayName = doc.data().displayName;
             userCreate.birthday = doc.data().birthday;
             userCreate.gender = doc.data().gender;
             userCreate.phoneNumber = doc.data().phoneNumber;
             console.log(userCreate);
             setUserCustom(userCreate);
-            
         };
 
         fetchData();
     }, []);
-    
+
     return (
         <SafeAreaLayout style={styles.container} insets='top'>
             <TopNavigation
